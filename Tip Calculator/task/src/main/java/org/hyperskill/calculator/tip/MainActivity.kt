4a52632b584bf3485564ea520bc1188363eebaf9
renniej <ouroboric2023@proton.me>
2024-06-18 19:00:44 -0400

    finished project

--- Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
+++ Tip Calculator/task/src/main/java/org/hyperskill/calculator/tip/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import org.hyperskill.calculator.tip.databinding.ActivityMainBinding
 import java.math.BigDecimal
+import java.math.BigInteger
 import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
@@ -40,17 +41,34 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+
+    private fun getTipAmount() : BigDecimal{ //assumes validNumber
+
+
+        val percentage = BigDecimal(binding.seekBar.progress.toDouble() / 100.toDouble())
+        val amount = BigDecimal(binding.editText.text?.toString())
+
+
+        return (percentage * amount).setScale(2,RoundingMode.HALF_EVEN)
+    }
+
     private fun  refreshTextViews() {
         val num = binding.editText.text?.toString()?.toBigDecimalOrNull() ?: BigDecimal.ZERO
         val validNumber =  (num > BigDecimal.ZERO)
 
+
         if (validNumber) {
             binding.billValueTv.text = "Bill Value: $${num.setScale(2, RoundingMode.HALF_EVEN)}"
             binding.tipPercentTv.text =   "Tip: ${binding.seekBar.progress}%"
+            binding.tipAmountTv.text = "Tip Amount: $" +
+                    "${String.format("%.2f", binding.editText.text.toString().toBigDecimal()
+                            * 0.01.toBigDecimal()
+                            * binding.seekBar.progress.toBigDecimal())}"//"Tip Amount: $${String.format("%.2f", getTipAmount())}"
         }
         else {
             binding.billValueTv.text = ""
             binding.tipPercentTv.text = ""
+            binding.tipAmountTv.text = ""
         }
 
 
